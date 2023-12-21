ca67fb1f71269b9f91ef63051a27c42f7a658427
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-21 15:43:32 -0600

    storing

---
+++ Exercise02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("What is the input string? ");
+var input = Console.ReadLine();
+Console.Write($"{input} has {input.Length} characters.");
+
---
+++ Exercise03/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("What is the quote? ");
+var quote = Console.ReadLine();
+Console.Write("Who said it? ");
+var author = Console.ReadLine();
+
+Console.WriteLine($"{author} says, \"{quote}\"");
+
