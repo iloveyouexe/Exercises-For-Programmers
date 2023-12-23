bb89c1f2e6287820fda45aba843d1c2a7f94121a
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-22 19:57:16 -0600

    started 8

---
+++ Exercise08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("How many people? ");
+double numberOfPeople = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("How many pizzas do you have? ");
+double numberOfPizzas = Convert.ToDouble(Console.ReadLine());
+
+double numberOfPizzasPerPerson = numberOfPizzas / numberOfPeople;
+double remainingSlices = numberOfPizzas - numberOfPizzasPerPerson;
+
+Console.WriteLine($"{numberOfPeople} people with {numberOfPizzas} pizzas. ");
+Console.WriteLine($"Each person gets {numberOfPizzasPerPerson} pieces of pizza." );
+Console.Write($"There are {remainingSlices} leftover pieces.");
