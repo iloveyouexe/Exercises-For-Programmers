7881f66c5a4c905ddcb16f17a7849da2063a6c4d
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-22 19:42:22 -0600

    finished 7

---
+++ Exercise07/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("What is the length of the room in feet? ");
+double lengthOfRoomInFeet = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("What is the width of the room in feet? ");
+double widthOfRoomInFeet = Convert.ToDouble(Console.ReadLine());
+
+double areaOfRoomInSquareFeet = lengthOfRoomInFeet * widthOfRoomInFeet;
+double areaOfRoomInSquareMeters = areaOfRoomInSquareFeet * 0.09290304; //formula m2 = f2 × 0.09290304
+
+
+Console.WriteLine($"You entered the dimensions of {lengthOfRoomInFeet} feet by {widthOfRoomInFeet} feet.");
+Console.Write("The area is ");
+Console.WriteLine($"{areaOfRoomInSquareFeet} square feet. ");
+Console.WriteLine($"{areaOfRoomInSquareMeters} square meters. ");
