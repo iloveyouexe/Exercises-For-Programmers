7a34586a8864fca79634c4c6f7126d7ca0b3af29
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-23 14:41:45 -0600

    finished some of 9, going to add L shaped room functionality.

--- Exercise09/Program.cs
+++ Exercise09/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Lets calculate the aomunt of gallons of paint needed for the ceiling of a room.");
+Console.Write("What is the length of the room in FT? ");
+double lengthOfRoom = Convert.ToDouble(Console.ReadLine());
+Console.Write("What is the width of the room in FT? ");
+double widthOfRoom = Convert.ToDouble(Console.ReadLine());
+double squareFeetOfRoom = lengthOfRoom * widthOfRoom;
+double gallonsOfPaint = Math.Ceiling(squareFeetOfRoom / 350);
+// assuming a gallon covers 350 sq ft.
+Console.WriteLine($"You will need to purchase {gallonsOfPaint} gallons of paint to cover {squareFeetOfRoom} square feet.");
