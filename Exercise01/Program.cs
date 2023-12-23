69d18d70be74db7d5794f05f2d09e897e103756b
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-22 18:32:37 -0600

    Added Day01

---
+++ Exercise01/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var name = "Brian";
+
+Console.Write("What is your name?");
+Console.WriteLine(name);
+Console.WriteLine($"Hello, {name}, nice to meet you!");
