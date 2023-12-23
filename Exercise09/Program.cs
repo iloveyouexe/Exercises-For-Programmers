6d081ff1a4b512a8440657f3f5dfd47d0e72c902
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-23 14:45:16 -0600

    finished 9

--- Exercise09/Program.cs
+++ Exercise09/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Lets calculate the aomunt of gallons of paint needed for the ceiling of a room.");
+﻿Console.WriteLine("Lets calculate the amount of gallons of paint needed for the ceiling of a room.");
 Console.Write("What is the length of the room in FT? ");
 double lengthOfRoom = Convert.ToDouble(Console.ReadLine());
 Console.Write("What is the width of the room in FT? ");
@@ -7,3 +7,4 @@ double squareFeetOfRoom = lengthOfRoom * widthOfRoom;
 double gallonsOfPaint = Math.Ceiling(squareFeetOfRoom / 350);
 // assuming a gallon covers 350 sq ft.
 Console.WriteLine($"You will need to purchase {gallonsOfPaint} gallons of paint to cover {squareFeetOfRoom} square feet.");
+
