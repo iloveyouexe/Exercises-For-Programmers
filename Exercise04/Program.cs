ad70222dc25580f27fbf9da54db24867ddce7743
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-22 18:39:35 -0600

    Finished Exercise 05

---
+++ Exercise04/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter a noun: ");
+var noun = Console.ReadLine();
+Console.Write("Enter a verb: ");
+var verb = Console.ReadLine();
+Console.Write("Enter an adjective: ");
+var adjective = Console.ReadLine();
+Console.Write("Enter an adverb: ");
+var adverb = Console.ReadLine();
+Console.Write($"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!");
+
