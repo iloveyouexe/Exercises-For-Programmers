c3e36ec18c757bc9a00d147cd0ddf42cfd042932
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-25 19:21:45 -0600

    finished 11

---
+++ Exercise11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("How many euros are you exchanging? ");
+double euroAmount = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("What is the exchange rate? ");
+double exchangeRate = Convert.ToDouble(Console.ReadLine());
+
+double usdAmount = euroAmount * exchangeRate;
+usdAmount = Math.Round(usdAmount / 100, 2); 
+
+Console.WriteLine($"{euroAmount} euros at an exchange rate of {exchangeRate} is {usdAmount:F2} U.S. dollars.");
