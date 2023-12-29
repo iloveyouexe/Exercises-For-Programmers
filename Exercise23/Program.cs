9a2c2a88624818fe976946f38e21e212b50a3e4d
Hunter Hamilton <hunter.hamilton@secturasoft.com>
2023-12-29 15:44:08 -0600

    started 23

---
+++ Exercise23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Is the car silent when you turn the key? ");
+string? isCarSilent = Convert.ToString(Console.ReadLine());
+
+if (isCarSilent == "y")
+{
+    Console.Write("Are the battery terminals corroded? ");
+    string? areTerminalsCorroded = Convert.ToString(Console.ReadLine());
+
+    if (areTerminalsCorroded == "y")
+    {
+        Console.WriteLine("Clean terminals and try starting again.");
+    } else if (areTerminalsCorroded == "n")
+    {
+        Console.WriteLine("Replace cables and try again.");
+    }
+} else if (isCarSilent == "n")
+{
+    
+}
