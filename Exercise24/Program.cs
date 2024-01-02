e3363444b5e65eb7493f1842e93666daa5329500
iloveyou.exe <iloveyouexe1@gmail.com>
2024-01-02 12:40:17 -0600

    fixed

--- Exercise24/Program.cs
+++ Exercise24/Program.cs
@@ -1,12 +1,14 @@
-﻿class Program
+﻿namespace Exercise24;
+
+static class Program
 {
     static void Main()
     {
         Console.WriteLine("Enter two strings and I'll tell you if they are anagrams: ");
         Console.Write("Enter the first string: ");
-        string firstString = Console.ReadLine();
+        string? firstString = Console.ReadLine();
         Console.Write("Enter the second string: ");
-        string secondString = Console.ReadLine();
+        string? secondString = Console.ReadLine();
 
         bool isAnagram = IsAnagram(firstString, secondString);
 
@@ -20,44 +22,46 @@
         }
     }
 
-    static bool IsAnagram(string first, string second)
+    static bool IsAnagram(string? first, string? second)
     {
-        if (first.Length != second.Length)
+        if (second != null && first != null && first.Length != second.Length)
         {
             return false;
         }
 
         Dictionary<char, int> charCount = new Dictionary<char, int>();
 
-        foreach (char c in first)
-        {
-            if (charCount.ContainsKey(c))
+        if (first != null)
+            foreach (char c in first)
             {
-                charCount[c]++;
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
             }
-            else
-            {
-                charCount[c] = 1;
-            }
-        }
-        
-        foreach (char c in second)
-        {
-            if (charCount.ContainsKey(c))
-            {
-                charCount[c]--;
 
-                if (charCount[c] < 0)
+        if (second != null)
+            foreach (char c in second)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]--;
+
+                    if (charCount[c] < 0)
+                    {
+                        return false;
+                    }
+                }
+                else
                 {
                     return false;
                 }
             }
-            else
-            {
-                return false; 
-            }
-        }
-        
+
         foreach (var pair in charCount)
         {
             if (pair.Value != 0)
