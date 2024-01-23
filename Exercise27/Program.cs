ded5e18c2deb44602c207eb9f584259ca3503e87
iloveyou.exe <iloveyouexe1@gmail.com>
2024-01-22 23:19:11 -0600

    stage

---
+++ Exercise27/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+Console.Write("Enter the first name: ");
+string? firstName = Convert.ToString(Console.ReadLine());
+
+Console.Write("Enter the last name: ");
+string? lastName = Convert.ToString(Console.ReadLine());
+
+Console.Write("Enter the ZIP code: ");
+double zipCode = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter an Employee ID: ");
+string? employeeId = Convert.ToString(Console.ReadLine());
+
