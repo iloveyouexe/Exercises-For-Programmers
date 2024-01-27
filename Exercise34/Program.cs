34c79c2dad88773464129fcf39dbed0e498907cb
iloveyou.exe <iloveyouexe1@gmail.com>
2024-01-26 23:21:08 -0600

    36

--- Exercise34/Program.cs
+++ Exercise34/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class program
+{
+    static void Main()
+    {
+        List<string> employees = new List<string>
+        {
+            "John Smith",
+            "Jackie Jackson",
+            "Chris Jones",
+            "Amanda Cullen",
+            "Jeremy Goodwin"
+        };
+        
+        Console.WriteLine($"There are {employees.Count} employees. ");
+        foreach (var employee in employees)
+        {
+            Console.WriteLine(employee);
+        }
+        
+        Console.WriteLine("Enter an employee name to remove: ");
+        string employeeToRemove = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+        bool removed = employees.Remove(employeeToRemove);
+
+        if (removed)
+        {
+            Console.WriteLine($"$There are now {employees.Count} employees: ");
+            
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            } 
+            {
+                Console.Write($"Employee '{employeeToRemove}' has been removed from the list. ");
+            }
+        }
+        Console.ReadLine();
+    }
+
+}
