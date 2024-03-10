a1f7686256449abadbcd9c91055e882173679bd1
iloveyouexe <bmo@soak.sh>
2024-03-09 22:37:27 -0500

    more

--- Exercise46/Program.cs
+++ Exercise46/Program.cs
@@ -3,5 +3,22 @@
     static void Main()
     {
         Console.Write("Future ex 46");
+        Console.WriteLine("What would you like to test");
+        var testingMeasure = Console.ReadLine();
+
+        object teastureMeasure = null;
+        switch (teastureMeasure)
+        {
+                var teasureMeasure = "";
+                break;
+
+            case
+                "Register":
+            {
+                Console.WriteLine("you've selected register");
+                break;
+            }
+        }
+        Console.WriteLine("Testing done");
     }
 }
